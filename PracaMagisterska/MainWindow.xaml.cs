13d96e2a884543e237a11ebf467d46a9fd9c0a9d
igaKarbowiak <221275@student.pwr.edu.pl>
2018-10-22 14:44:28 +0200

    Add connections between model and gui

--- PracaMagisterska/MainWindow.xaml.cs
+++ PracaMagisterska/MainWindow.xaml.cs
@@ -25,13 +25,115 @@ namespace PracaMagisterska
             InitializeComponent();
         }
 
-        private void Start1_Click(object sender, RoutedEventArgs e)
+        private void start_Click(object sender, RoutedEventArgs e)
         {
-            Model1 model1 = new Model1(20, 2, 3);
-            double[] re = model1.Flow(50);
-            Console.WriteLine(re[0]);
-            Console.WriteLine(re[1]);
+
+            double[] validateRes = validate_boxes();
+
+            double neuronLength = validateRes[0];
+            double denAxdiam = validateRes[1];
+            double speed = validateRes[2];
+            double time = validateRes[3];
+
+            if ((neuronLength > 0 ) && ( denAxdiam >0) && (speed > 0))
+            {
+
+
+                Model1 model1 = new Model1(neuronLength, denAxdiam, speed);
+                double[] re = model1.Flow(time);
+                M1TimeBlock.Text = re[0].ToString("0.##");
+                M1VolumeBlock.Text = re[1].ToString("0.##");
+
+                Model2 model2 = new Model2(neuronLength, denAxdiam, speed);
+                double[] re2 = model2.Flow(time);
+                M2TimeBlock.Text = re2[0].ToString("0.##");
+                M2VolumeBlock.Text = re2[1].ToString("0.##");
+
+                Model3 model3 = new Model3(neuronLength, denAxdiam, speed);
+                double[] re3 = model3.Flow(time);
+                M3TimeBlock.Text = re3[0].ToString("0.##");
+                M3VolumeBlock.Text = re3[1].ToString("0.##");
+
+            }
+
         }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            M1TimeBlock.Text = "";
+            M2TimeBlock.Text = "";
+            M3TimeBlock.Text = "";
+
+            M1VolumeBlock.Text = "";
+            M2VolumeBlock.Text = "";
+            M3VolumeBlock.Text = "";
+
+            neuronLenBox.Text = "";
+            denAxDiamBox.Text = "";
+            speedBox.Text = "";
+            timeBox.Text = "";
+        }
+
+        private double[] validate_boxes()
+        {
+            double neuronLength = 0;
+            double denAxdiam = 0;
+            double speed = 0;
+            double time = 0;
+
+            if (String.IsNullOrEmpty(neuronLenBox.Text) || denAxDiamBox.Text.Contains('.') || (Double.Parse(neuronLenBox.Text) < 30))
+            {
+                neuronLenBox.BorderBrush = System.Windows.Media.Brushes.Red;
+                infoNeuron.Foreground = Brushes.Red;
+            }
+
+            else
+            {
+                neuronLenBox.BorderBrush = System.Windows.Media.Brushes.Gray;
+                infoNeuron.Foreground = Brushes.Black;
+                neuronLength = Int32.Parse(neuronLenBox.Text);
+            }
+
+            if (String.IsNullOrEmpty(denAxDiamBox.Text) || denAxDiamBox.Text.Contains('.') || (0.3 > Double.Parse(denAxDiamBox.Text)) || (Double.Parse(denAxDiamBox.Text) > 0.5))
+            {
+                denAxDiamBox.BorderBrush = System.Windows.Media.Brushes.Red;
+                denAxDiamInfo.Foreground = Brushes.Red;
+            }
+            else
+            {
+                denAxDiamBox.BorderBrush = System.Windows.Media.Brushes.Gray;
+                denAxDiamInfo.Foreground = Brushes.Black;
+                denAxdiam = Double.Parse(denAxDiamBox.Text);
+            }
+
+            if (speedBox.Text.Contains('.') || String.IsNullOrEmpty(speedBox.Text) || (Double.Parse(speedBox.Text) < 0))
+            {
+                speedBox.BorderBrush = System.Windows.Media.Brushes.Red;
+            }
+            else
+            {
+                speedBox.BorderBrush = System.Windows.Media.Brushes.Gray;
+                speed = Double.Parse(speedBox.Text);
+            }
+
+
+            if (timeBox.Text.Contains('.') || String.IsNullOrEmpty(timeBox.Text) || (Double.Parse(timeBox.Text) < 0))
+            {
+                timeBox.BorderBrush = System.Windows.Media.Brushes.Red;
+            }
+            else
+            {
+                timeBox.BorderBrush = System.Windows.Media.Brushes.Gray;
+                time = Double.Parse(timeBox.Text);
+            }
+
+            double[] res = { neuronLength, denAxdiam, speed, time };
+            return res;
+
+        }
+
     }
+
+
 
 }
--- PracaMagisterska/Model1.cs
+++ PracaMagisterska/Model1.cs
@@ -6,29 +6,118 @@ using System.Threading.Tasks;
 
 namespace PracaMagisterska
 {
-    public class Model1 
+    public abstract class Model
     {
-        public int length;
-        public int speed;
-        public double surface;
+        public double length;
+        public double speed;
+        public double input_surface;
 
-        public Model1(int lgth, int diam, int sp)
+        public abstract double[] Flow(double time);
+    }
+    public class Model1 : Model
+    {
+        public Model1(double lgth, double diam, double sp)
         {
             this.length = lgth;
-            int diameter = diam;
+            double diameter = diam;
             this.speed = sp;
-            this.surface = Math.Pow((diameter / 2), 2) * Math.PI;
-            double volume = this.surface * length;
+            this.input_surface = Math.Pow((diameter / 2), 2) * Math.PI;
+            double volume = this.input_surface * length;
         }
 
-        public double[] Flow(int time)
+        public override double[] Flow(double time)
         {
             // Time after which liquid will begin to flow out
             double outFlowTime = this.length / this.speed;
 
+            if (outFlowTime > time)
+            {
+                double[] res0 = { 0, 0 };
+                return res0;
+            }
+
             // The volume of liquid which flowed up
-            double flowedVolume = this.surface * this.speed * (time - outFlowTime);
+            double flowedVolume = this.input_surface * this.speed * (time - outFlowTime);
             double[] res = { outFlowTime, flowedVolume };
+            return res;
+        }
+    }
+
+    public class Model2 : Model
+    {
+        public double dendrite_length, axon_length, soma_diameter;
+        public double dendrite_volume, soma_volume, axon_volume;
+        public double soma_treshold;
+
+        public Model2(double lgth, double diam, double sp)
+        {
+            this.length = lgth;
+            double diameter = diam;
+            this.speed = sp;
+
+            calculate_parameters(diameter);
+
+            //Console.WriteLine(this.dendrite_length);
+            //Console.WriteLine(this.axon_length);
+            //Console.WriteLine(this.soma_diameter);
+            //Console.WriteLine(this.soma_treshold);
+            //Console.WriteLine(this.soma_volume);
+  
+        }
+
+        public void calculate_parameters(double diameter)
+        {
+            this.dendrite_length = this.length / 26;
+            this.soma_diameter = this.length * 10 / 26;
+            this.axon_length = this.length * 20 / 26;
+            this.input_surface = Math.Pow((diameter / 2), 2) * Math.PI;
+            this.dendrite_volume = this.dendrite_length * this.input_surface;
+            this.axon_volume = this.axon_length * this.input_surface;
+            this.soma_volume = Math.Pow(this.soma_diameter / 2, 2) * Math.PI * this.soma_diameter;
+            this.soma_treshold = (this.soma_diameter / 2 - diameter / 2) * this.soma_volume / this.soma_diameter;
+        }
+
+        public override double[] Flow(double time)
+        {
+
+            double time_for_fill_soma = this.soma_treshold / (this.input_surface * this.speed);
+            Console.WriteLine("Time For Fill some");
+            Console.WriteLine(time_for_fill_soma);
+
+            // Time after which liquid will begin to flow out
+            double outFlowTime = this.length / this.speed + time_for_fill_soma;
+
+            if (outFlowTime > time)
+            {
+                double[] res0 = { 0, 0 };
+                return res0;
+            }
+
+            // The volume of liquid which flowed up
+            double flowedVolume = this.input_surface * this.speed * (time - outFlowTime);
+            double[] res = { outFlowTime, flowedVolume };
+            return res;
+        }
+    }
+
+    public class Model3 : Model2
+    {
+        public Model3(double lgth, double diam, double sp) : base(lgth, diam, sp) { }
+
+        public override double[] Flow(double time)
+        {
+            double time_for_fill_soma = this.soma_treshold / (this.input_surface * this.speed * 2);
+
+            double outFlowTime = (this.input_surface * (this.axon_length + this.soma_diameter)) / (this.input_surface * this.speed);
+
+            if (outFlowTime > time)
+            {
+                double[] res0 = { 0, 0 };
+                return res0;
+            }
+
+            double flowedVolume = this.input_surface * this.speed * (time - outFlowTime) * 2;
+            double[] res = { outFlowTime, flowedVolume};
             return res;
         }
     }
